94887500a66b2d585034a678d4e6c2b93002f93c
Palash <palashnath880@gmail.com>
2022-10-25 11:37:13 +0600

    Header Updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,14 +23,14 @@ const Header = () => {
     }, []);
 
     return (
-        <div ref={header} className={`z-50 ${sticky ? 'pb-0' : 'pt-5'} bg-base-100 w-full fixed top-0 left-0 duration-300 transition-all`}>
+        <div ref={header} className={`z-50 ${sticky ? 'pb-0 bg-base-100' : 'pt-5'} w-full fixed top-0 left-0 duration-300 transition-all`}>
             <div className='navbar container mx-auto'>
                 <div className="flex-1">
-                    <Link className="normal-case text-3xl" style={{ fontFamily: "'Lobster Two', cursive" }} to='/'>
+                    <Link className={`normal-case text-3xl ${sticky ? 'text-gray-700' : 'text-slate-50'}`} style={{ fontFamily: "'Lobster Two', cursive" }} to='/'>
                         e<span className='text-violet-500'>Shiki.com</span>
                     </Link>
                 </div>
-                <div className="flex-none">
+                <div className={`flex-none ${sticky ? 'text-gray-700' : 'text-slate-50'}`}>
                     <ul className="menu menu-horizontal p-0">
                         <li><Link className='hover:bg-transparent'>Home</Link></li>
                         <li><Link className='hover:bg-transparent'>About</Link></li>
@@ -38,14 +38,14 @@ const Header = () => {
                         <li><Link className='hover:bg-transparent'>FAQ</Link></li>
                     </ul>
                     <ul className="flex gap-2">
-                        <li><Link className='duration-300 border hover:bg-violet-500 hover:text-slate-50 py-2 px-4 rounded-full' to='/login'>Login</Link></li>
-                        <li><Link className='duration-300 border hover:bg-violet-500 hover:text-slate-50 py-2 px-4 rounded-full' to='/signup'>Signup</Link></li>
+                        <li><Link className='duration-300 border hover:bg-violet-500 hover:text-slate-50 hover:border-violet-500 py-2 px-4 rounded-full' to='/login'>Login</Link></li>
+                        <li><Link className='duration-300 border hover:bg-violet-500 hover:text-slate-50 hover:border-violet-500 py-2 px-4 rounded-full' to='/signup'>Signup</Link></li>
                     </ul>
                     <div className="dropdown dropdown-end mr-2">
                         <label tabIndex={0} className="btn btn-ghost btn-circle">
                             <div className="indicator">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                                <span className="badge badge-sm indicator-item">8</span>
+                                <span className={`badge badge-sm indicator-item ${sticky ? '' : 'bg-slate-50 text-gray-700'}`}>8</span>
                             </div>
                         </label>
                         <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
