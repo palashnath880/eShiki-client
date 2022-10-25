968a32d786a255688a1f4ed5f4d7468f73f79129
Palash <palashnath880@gmail.com>
2022-10-25 09:40:01 +0600

    Footer added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='bg-gray-700'>
+            <div className='mx-auto container'>
+                <div></div>
+                <div className='text-center text-slate-50 '>
+                    <p>Copyright 2020 - 2022 | Develop By Palash</p>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
